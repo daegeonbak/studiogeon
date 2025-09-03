ce7ca7d5a832ee17b31604fda9e61a139080cef5
daegeonbak <daegeonbak@gmail.com>
2025-09-03 17:50:36 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector(".subscribe-btn").addEventListener("click", () => {
+  alert("Subscribe 버튼을 클릭하셨습니다. 구독 페이지로 이동합니다!");
+  // 실제 페이지로 이동하고 싶다면 아래 줄 사용하세요:
+  // window.location.href = "subscribe.html";
+});
